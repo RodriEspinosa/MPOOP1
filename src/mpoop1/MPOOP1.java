859a543410baebba83dcdd56f04dbc14009575da
RodriEspinosa <143237276+RodriEspinosa@users.noreply.github.com>
2024-09-26 23:03:18 -0600

    Creación de práctica

---
+++ src/mpoop1/MPOOP1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mpoop1;
+
+/**
+ *
+ * @author Espinosa Ramirez Jose Rodrigo 
+ */
+public class MPOOP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                System.out.println("Hola mundo con ANT");
+        System.out.println("Desde POO G3");
+        int a = 3; //variables primitivas
+        Integer b = 3; //variables de objeto
+        String cadena = new String("hola"); //variable de referencia 
+        System.out.println(a); //parseo cambia de tipo de dato
+        System.out.println(b); //parseo 
+        System.out.println(cadena); //no hace conversion
+    }
+}
